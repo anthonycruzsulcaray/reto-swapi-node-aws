b623fd23a371386285910a2e8b0c610f3e93d03d
Anthony Cruz <ms_anthony_cruz@hotmail.com>
2024-09-05 13:11:19 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './serverlesDeploy/http/app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('API de Películas')
-    .setDescription('Documentación de la API de Películas')
-    .setVersion('1.0')
-    .addTag('peliculas')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-
   await app.listen(3000);
 }
+
 bootstrap();
--- src/serverlesDeploy/http/lambda.ts
+++ src/serverlesDeploy/http/lambda.ts
@@ -2,51 +2,73 @@ import { Handler, Context } from 'aws-lambda';
 import { Server } from 'http';
 import { createServer, proxy } from 'aws-serverless-express';
 import { eventContext } from 'aws-serverless-express/middleware';
-
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 const express = require('express');
 
-// NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
-// due to a compressed response (e.g. gzip) which has not been handled correctly
-// by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
-// binaryMimeTypes below
 const binaryMimeTypes: string[] = [];
 let cachedServer: Server;
 
+
+async function setupSwagger(app: INestApplication) {
+   const config = new DocumentBuilder()
+      .setTitle('API de Películas')
+      .setDescription('Documentación de la API de Películas')
+      .setVersion('1.0')
+      .addTag('peliculas')
+      .build();
+   const document = SwaggerModule.createDocument(app, config);
+   SwaggerModule.setup('docs', app, document);
+}
+
+
+
 async function bootstrapServer(): Promise<Server> {
    if (!cachedServer) {
-      const expressApp = express();
-      const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp), { cors: true })
-      nestApp.use(eventContext());
-      // nestApp.setGlobalPrefix('starwars-api')
-      await nestApp.init();
-      cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+      try {
+         const expressApp = express();
+         const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp), { cors: true })
+         nestApp.use(eventContext());
+         // nestApp.setGlobalPrefix('starwars-api')
+         // Enable swagger
+         setupSwagger(nestApp)
+         await nestApp.init();
+         cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+      } catch (error) {
+         return Promise.reject(error);
+      }
    }
    return cachedServer;
 }
 
 export const handler: Handler = async (event: any, context: Context) => {
+   if (event.path === '/docs') {
+      event.path === '/docs'
+   }
+   // swaager
+   event.path = event.path.includes('swagger-ui') ? `/docs${event.path}` : event.path;
+   // 
    cachedServer = await bootstrapServer();
-   console.log("REQUEST::",JSON.stringify(event))
-   console.log("REQUEST::RESOURCE::",event.resource)
-   console.log("REQUEST::pathParameters::",event.pathParameters)
+   console.log("REQUEST::", JSON.stringify(event))
+   console.log("REQUEST::RESOURCE::", event.resource)
+   console.log("REQUEST::pathParameters::", event.pathParameters)
    if (
       event.body &&
       getContentType(event)
    ) {
       //event.body = (Buffer.from(event.body, 'binary') as unknown) as string;  
-      console.log("REQUEST::BODY::",'binary')
-   }else{
-      console.log("REQUEST::BODY::",event.body)
+      console.log("REQUEST::BODY::", 'binary')
+   } else {
+      console.log("REQUEST::BODY::", event.body)
    }
-   const response=await proxy(cachedServer, event, context, 'PROMISE').promise;
-   console.log("REPONSE::",JSON.stringify(response))
+   const response = await proxy(cachedServer, event, context, 'PROMISE').promise;
+   console.log("REPONSE::", JSON.stringify(response))
    return response
 }
-function getContentType(event: any){
+function getContentType(event: any) {
    try {
       return event.headers['Content-Type'].includes('multipart/form-data')
    } catch (error) {
